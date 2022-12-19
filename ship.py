ec7b4f9636cd3033871545547d01d5d8e263856f
Slava Yanuskevich <cool.9nyw@gmail.com>
2022-12-19 19:05:53 +0300

    add ship speed

--- ship.py
+++ ship.py
@@ -1,11 +1,13 @@
 import pygame
 
 
+
 class Ship():
     """ Класс для управления кораблём. """
     def __init__(self, ai_game):
         """ Иницилизиирует корабль и задаёт его начальную позицию. """
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # загружает изображение корабля и получает прямоугольник.
@@ -13,16 +15,22 @@ class Ship():
         self.rect = self.image.get_rect()
         # Каждый новый корабль появляется у нижнего края экрана.
         self.rect.midbottom = self.screen_rect.midbottom
+        # Сохранение вещественной координаты центра коробля.
+        self.x = float(self.rect.x)
         # флаг перемещения
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """ Обновляет позцию корабля с учётом флага. """
+
+        # Обновляется атрибут х, не rect.
         if self.moving_right:
-            self.rect.x += 1
+            self.rect.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.rect.x -= self.settings.ship_speed
+        # # Обновление атрибута rect на основании self.x.
+        # self.rect.x = self.x
 
     def blitme(self):
         """ Рисует корабль в текущей позиции. """
