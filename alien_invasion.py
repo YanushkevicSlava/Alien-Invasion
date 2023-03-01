333d14fad1ece1fb554f7e46e5b29ac1d5181a2d
Slava Yanuskevich <cool.9nyw@gmail.com>
2023-03-01 13:39:37 +0300

    add end game

--- alien_invasion.py
+++ alien_invasion.py
@@ -74,9 +74,12 @@ class AlienInvasion:
         """ Запуск основного цикла """
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+
             self._update_screen()
 
     def _check_events(self):
@@ -125,19 +128,22 @@ class AlienInvasion:
 
     def _ship_hit(self):
         """Обрабатывает столкновение корабля с пришельцем."""
-        # Уменьшение ship_left.
-        self.stats.ships_left -= 1
+        if self.stats.ships_left > 0:
+            # Уменьшение ship_left.
+            self.stats.ships_left -= 1
 
-        # Очистка списков пришельцев от снарядов.
-        self.aliens.empty()
-        self.bullets.empty()
+            # Очистка списков пришельцев от снарядов.
+            self.aliens.empty()
+            self.bullets.empty()
 
-        # Создание нового флота и размещение корабля в центре.
-        self._create_fleet()
-        self.ship.center_ship()
+            # Создание нового флота и размещение корабля в центре.
+            self._create_fleet()
+            self.ship.center_ship()
 
-        # Пауза.
-        sleep(0.5)
+            # Пауза.
+            sleep(0.5)
+        else:
+            self.stats.game_active = False
 
     def _update_bullets(self):
         """ Обновляет позиции снарядов и уничтожает старые снаряды. """
