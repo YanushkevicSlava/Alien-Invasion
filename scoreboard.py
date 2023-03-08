405bd692ea78a11669ad72ca7201dc1571c1041e
Slava Yanuskevich <cool.9nyw@gmail.com>
2023-03-08 21:27:40 +0300

    fix play level

--- scoreboard.py
+++ scoreboard.py
@@ -17,14 +17,14 @@ class Scoreboard:
         # Подготовка изображения сётов.
         self.prep_score()
         self.prep_high_score()
-        self.prep_level
+        self.prep_level()
 
     def prep_score(self):
         """Преобразует текущий счёт в изображение."""
         rounded_score = round(self.stats.score, -1)
         score_str = "{:,}".format(rounded_score)
         self.score_image = self.font.render(score_str, True,
-                                            self.text_color, (230, 230, 230))
+                                            self.text_color, self.settings.bg_color)
 
         # Вывод счёта в правой верхней части экрана.
         self.score_rect = self.score_image.get_rect()
@@ -42,7 +42,7 @@ class Scoreboard:
         high_score = round(self.stats.high_score, -1)
         high_score_str = "{:,}".format(high_score)
         self.high_score_image = self.font.render(high_score_str, True, self.text_color,
-                                                 (230, 230, 230))
+                                                 self.settings.bg_color)
         # Рекорд выравнивается по центру верхней стороны.
         self.high_score_rect = self.high_score_image.get_rect()
         self.high_score_rect.centerx = self.screen_rect.centerx
@@ -57,8 +57,7 @@ class Scoreboard:
     def prep_level(self):
         """Преобразует уровень в графическое изображение."""
         level_str = str(self.stats.level)
-        self.level_image = self.font.render(level_str, True,
-                                            self.text_color, (230, 230, 230))
+        self.level_image = self.font.render(level_str, True, self.text_color, self.settings.bg_color)
         # Уровень выводится под текущим счётом.
         self.level_rect = self.level_image.get_rect()
         self.level_rect.right = self.score_rect.right
